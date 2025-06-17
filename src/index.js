cb2b987199f1990ea8e87691ba8c88f31b83d953
Mark <mcschaefer26@gmail.com>
2025-06-17 16:52:26 +0200

    Prompting the user client-side for the task details.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Starting the jsl 2 js script
+
+// Save this data in an object or array and loop through when needed
+const tasks = [];
+// Promt the user to enter title, description, and status for 2 seperate tasks
+prompt('Please enter the title of the task');
+prompt('Please enter the description of the task');
+prompt('Please enter the status of the task');
